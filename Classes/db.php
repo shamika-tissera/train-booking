fa7d559358a9cbc2fa93303f7afd10a11a8ba4d8
LasithEranga <lasitheranga1@gmail.com>
2021-09-23 13:22:48 +0530

    Heading update

--- Classes/db.php
+++ Classes/db.php
@@ -2,11 +2,12 @@
 
 $servername = "mysql-51512-0.cloudclusters.net";
 $username = "admin";
-$password ="10177";
+$password ="YZ7mSIgV";
+$port = '10177';
 $dbname = "ted";
 
 // Create the conncection
-$conn = new mysqli($servername,$username,$password,$dbname);
+$conn = new mysqli($servername,$port,$username,$password,$dbname);
 
 //check connection
 if($conn->connect_error){
