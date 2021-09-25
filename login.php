6dcb42d3ff6c405651247f7a6c82298252c63bcf
LasithEranga <lasitheranga1@gmail.com>
2021-09-25 18:16:03 +0530

    Final

--- login.php
+++ login.php
@@ -19,8 +19,8 @@ include("includes/db.php");
 </head>
 
 <body class="bg-gradient-primary">
-  <div class="container">
-    <div class="row justify-content-center">
+  <div class="container ">
+    <div class="row justify-content-center ">
       <div class="col-md-9 col-lg-12 col-xl-10">
         <div class="card shadow-lg o-hidden border-0 my-5">
           <div class="card-body p-0">
